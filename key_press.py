70897a7ceedf95ae39714cf8185ef924c02053ed
vitkalovav@gmail.com <arrows364574>
2018-11-11 21:50:15 +0000

    keyboard press read

--- key_press.py
+++ key_press.py
@@ -1,9 +1,57 @@
-from curtsies import Input
+# from curtsies import Input
+#
+# def main():
+#     with Input(keynames='curses') as input_generator:
+#         for e in input_generator:
+#             print(repr(e))
+#
+# if __name__ == '__main__':
+#     main()
 
-def main():
-    with Input(keynames='curses') as input_generator:
-        for e in input_generator:
-            print(repr(e))
+# !/usr/bin/python3
 
-if __name__ == '__main__':
-    main()
+# adapted from https://github.com/recantha/EduKit3-RC-Keyboard/blob/master/rc_keyboard.py
+
+import sys, termios, tty, os, time
+
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+button_delay = 0.2
+
+while True:
+    char = getch()
+
+    if (char == "p"):
+        print("Stop!")
+        exit(0)
+1
+    if (char == "a"):
+        print("Left pressed")
+        time.sleep(button_delay)
+
+    elif (char == "d"):
+        print("Right pressed")
+        time.sleep(button_delay)
+
+    elif (char == "w"):
+        print("Up pressed")
+        time.sleep(button_delay)
+
+    elif (char == "s"):
+        print("Down pressed")
+        time.sleep(button_delay)
+
+    elif (char == "1"):
+        print("Number 1 pressed")
+        time.sleep(button_delay)
